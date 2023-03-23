7065c319b0380f9e60c015f57261680ce55cd8a3
Ki-Joon PARK <glycerine@live.co.kr>
2023-03-23 12:07:38 +0900

    add: 2.5 쿼리 메소드

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,8 +3,21 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // JpaRepository 를 상속받는 ItemRepository 작성.
 // JpaRepository 는 2개의 제네릭 타입을 사용하는데 첫 번째에는 엔티티 타입 클래스를 넣어주고, 두 번째는 기본키 타입을 넣어줌.
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    // 쿼리 메소드를 이용할 때 가장 많이 사용하는 문법으로 find 사용
+    // 엔티티의 이름은 생략이 가능하며, By 뒤에는 검색할 때 사용할 변수의 이름 작성
+    // find + (엔티티이름) + By + 변수 이름
+    List<Item> findByItemNm(String itemNm);
 
+    // 상품을 상품명과 상품 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // 파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 // 통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션
 // 실제 애플리케이션을 구동할 때처럼 모든 Bean 을 IoC 컨테이너에 등록
@@ -35,5 +36,72 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    // 테스트 코드 실행 시 데이터베이스에 상품 데이터가 없으므로 테스트 데이터 생성을 위해 10개의 삼품을 저장하는 메소드를 작성하여 findByItemNmTest()에서 실행
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        // 상품을 만드는 메소드 실행하여 조회할 대상 생성
+        this.createItemList();
+
+        // ItemRepository 인터페이스에 작성했던 findByItemNm 메소드를 호출. 파라미터로는 "테스트 상품1"이라는 상품명을 전달
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        
+        // 조회 결과 얻은 item 객체들을 출력
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        // 상품을 만드는 메소드 실행하여 조회할 대상 생성
+        this.createItemList();
+
+        // 상품명이 "테스트 상품1" 또는 상품 상세 설명이 "테스트 상품 상세 설명5"이면 해당 상품을 itemList 에 할당
+        // 테스트 코드를 실행하면 조건대로 2개의 상품이 출력되는 것을 확인
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+
+        // 현재 데이터베이스에 저장된 가격은 10001~10010. 테스트 코드 실행 시 10개의 상품을 저장하는 로그가 콘솔에 나타나고 마지막에 가격이 10005보다 작은 4개의 상품을 출력
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
